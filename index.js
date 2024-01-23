d2583e762e3c807cc524e33804a6acd480922fa4
anushka <anushkanil1@gmail.com>
2024-01-22 22:30:43 -0800

    added styles
    Added More styles

---
+++ index.js
@@ -0,0 +1,14 @@
+let tipTotal=0.0
+let total=0.0
+let tipDisplay=document.getElementById('calculate')
+console.log(tipDisplay)
+function cal(){
+    
+ 
+    let amount=document.getElementById("amount").value
+    tipDisplay+=amount
+    
+    console.log(amount)
+}
+
+
