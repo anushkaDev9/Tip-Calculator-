ded5a35810e7e4d3b07e46c3401503a53e5cd2da
anushka <anushkanil1@gmail.com>
2024-01-23 16:15:17 -0800

    Responsive

--- index.js
+++ index.js
@@ -3,33 +3,30 @@ let amount  //total amount
  let cal
  let display
  let total
+ let p
+ let d
 function myFunction() { //onload function 
     cal=document.getElementById('calculate').innerText
     display=parseInt(cal.substring(1,cal.length))
     totalDisplay=document.getElementById('total').innerText
     total=parseInt(totalDisplay.substring(1,totalDisplay.length))
-
   }
-function  myFunctions(){
-  amount=parseFloat(document.getElementById('amount').value)
-  display+=amount
-  total+=amount
+  function addPercentage(n){
+    p=n/100
+    d=1+p
+  console.log(d)
   }
-function addPercentage(n){
-    let p=n/100
-    let d=1+p
-     display*=p
-     total*=d
-    console.log(d)
-}
 function cals(){
-    num=parseInt(document.getElementById('num').value)
-
-let n = (display/num)
+  amount=parseFloat(document.getElementById('amount').value)
+  num=parseInt(document.getElementById('num').value)
+let n = (amount*p)/num
 n=n.toFixed(2)
-
-total=(total/num).toFixed(2)
+total=((amount*d)/num).toFixed(2)
 console.log(n)
  document.getElementById('calculate').innerText='$'+n
  document.getElementById('total').innerText='$'+total
+}
+function reset(){
+  document.getElementById('calculate').innerText='$0.00'
+ document.getElementById('total').innerText='$0.00'
 }
